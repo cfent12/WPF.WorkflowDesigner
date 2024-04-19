be1b936e6f238a7b7f017fd509df7f59a8ffe7fe
HRH <cfent12@gmail.com>
2024-04-19 15:25:42 +0900

    workflow 편집기 프로젝트 추가 (99) workflowdesigner 수정 (1)  - Save, Run 수정  - Load 추가

--- 01.WorkflowDesignerApp/WorkflowDesignerApp/MainWindow.xaml.cs
+++ 01.WorkflowDesignerApp/WorkflowDesignerApp/MainWindow.xaml.cs
@@ -22,6 +22,9 @@ using System.Diagnostics;
 using System.Xaml;
 using ExpressionTextBox;
 using System.Activities.Presentation.View;
+using System.Reflection;
+using System.IO;
+using System.Activities.XamlIntegration;
 
 namespace WorkflowDesignerApp
 {
@@ -46,25 +49,32 @@ namespace WorkflowDesignerApp
         private void AddDesigner()
         {
             // Create an instance of WorkflowDesigner class.
-            this.wd = new WorkflowDesigner();
+            wd = new WorkflowDesigner();
 
             // Place the designer canvas in the middle column of the grid.
-            Grid.SetColumn(this.wd.View, 1);
+            Grid.SetColumn(wd.View, 1);
 
             // Load a new Flowchart as default.
-            this.wd.Load(new Flowchart());
-
-            // 인수 탭 추가
-            var designerView = wd.Context.Services.GetService<DesignerView>();
-            designerView.WorkflowShellBarItemVisibility =
-                ShellBarItemVisibility.Variables |
-                ShellBarItemVisibility.Arguments |  // 인수 탭
-                ShellBarItemVisibility.Imports |
-                ShellBarItemVisibility.Zoom |
-                ShellBarItemVisibility.MiniMap;
+            var builder = new ActivityBuilder
+            {
+                Name = "ActivityBuilder",
+                Implementation = new Flowchart()
+            };
+            // Add Argument - Name에 Space 포함시 주의 발생
+            builder.Properties.Add(new DynamicActivityProperty { Name = "BuilderName", Type = typeof(InArgument<string>) });
+            wd.Load(builder);
 
             // Add the designer canvas to the grid.
-            grid1.Children.Add(this.wd.View);
+            grid1.Children.Add(wd.View);
+
+            // 인수 탭 강제 추가
+            //var designerView = wd.Context.Services.GetService<DesignerView>();
+            //designerView.WorkflowShellBarItemVisibility =
+            //    ShellBarItemVisibility.Variables |
+            //    ShellBarItemVisibility.Arguments |  // 인수 탭
+            //    ShellBarItemVisibility.Imports |
+            //    ShellBarItemVisibility.Zoom |
+            //    ShellBarItemVisibility.MiniMap;
         }
 
         private void RegisterMetadata()
@@ -82,35 +92,76 @@ namespace WorkflowDesignerApp
             var ctrl = new ToolboxControl();
 
             // Create a category.
-            var category = new ToolboxCategory("category1");
+            var eisCategory = new ToolboxCategory("Custom");
+            var controlCategory = new ToolboxCategory("ControlCategory");
+            var flowCategory = new ToolboxCategory("FlowCategory");
+            var runtimeCategory = new ToolboxCategory("Runtime");
+            var primitivesCategory = new ToolboxCategory("Primitives");
+            var transation = new ToolboxCategory("Transaction");
+            var collection = new ToolboxCategory("Collection");
+            var errorHandling = new ToolboxCategory("Error Handling");
 
             // Create Toolbox items.
-            var tool1 =
-                new ToolboxItemWrapper("System.Activities.Statements.Assign",
-                typeof(Assign).Assembly.FullName, null, "Assign");
+            MultiAssign multiAssign = new MultiAssign();
+            MyDynamicActivity myDynamicActivity = new MyDynamicActivity();
+              // ToolboxItemWrapper 생성 4가지 방법
+            var iDoWhile = new ToolboxItemWrapper(typeof(DoWhile));
+            var iForEach = new ToolboxItemWrapper(typeof(ForEach<>), "ForEach");
+            var iIf = new ToolboxItemWrapper(typeof(If), "icon-bitmap.png", "If");
+            var iParallel = new ToolboxItemWrapper("System.Activities.Statements.Parallel", typeof(Parallel).Assembly.FullName, null, "Parallel");
 
-            var tool2 = new ToolboxItemWrapper("System.Activities.Statements.Sequence",
-                typeof(Sequence).Assembly.FullName, null, "Sequence");
-
-            var tool3 = new ToolboxItemWrapper(typeof(WriteLine));
+            var iParallelForEach = new ToolboxItemWrapper(typeof(ParallelForEach<>));
+            var iPick = new ToolboxItemWrapper(typeof(Pick));
+            var iPickBranch = new ToolboxItemWrapper(typeof(PickBranch));
+            var iSequence = new ToolboxItemWrapper(typeof(Sequence));
+            var iSwitch = new ToolboxItemWrapper(typeof(Switch<>));
+            var iWhile = new ToolboxItemWrapper(typeof(While));
 
             // Add the Toolbox items to the category.
-            category.Add(tool1);
-            category.Add(tool2);
-            category.Add(tool3);
-
-            // Add Custom Control
-            var eisCategory = new ToolboxCategory("Custom");
-
-            MultiAssign multiAssign = new MultiAssign();
             eisCategory.Add(new ToolboxItemWrapper(multiAssign.GetType(), multiAssign.GetType().Name));
-
-            MyDynamicActivity myDynamicActivity = new MyDynamicActivity();
             eisCategory.Add(new ToolboxItemWrapper(myDynamicActivity.GetType(), myDynamicActivity.GetType().Name));
+            controlCategory.Add(iDoWhile);
+            controlCategory.Add(iForEach);
+            controlCategory.Add(iIf);
+            controlCategory.Add(iParallel);
+            controlCategory.Add(iParallelForEach);
+            controlCategory.Add(iPick);
+            controlCategory.Add(iPickBranch);
+            controlCategory.Add(iSequence);
+            controlCategory.Add(iSwitch);
+            controlCategory.Add(iWhile);
+            flowCategory.Add(new ToolboxItemWrapper(typeof(Flowchart)));
+            flowCategory.Add(new ToolboxItemWrapper(typeof(FlowDecision)));
+            flowCategory.Add(new ToolboxItemWrapper(typeof(FlowSwitch<>)));
+            runtimeCategory.Add(new ToolboxItemWrapper(typeof(Persist)));
+            runtimeCategory.Add(new ToolboxItemWrapper(typeof(TerminateWorkflow)));
+            primitivesCategory.Add(new ToolboxItemWrapper(typeof(Assign)));
+            primitivesCategory.Add(new ToolboxItemWrapper(typeof(Delay)));
+            primitivesCategory.Add(new ToolboxItemWrapper(typeof(InvokeMethod)));
+            primitivesCategory.Add(new ToolboxItemWrapper(typeof(WriteLine)));
+            transation.Add(new ToolboxItemWrapper(typeof(CancellationScope)));
+            transation.Add(new ToolboxItemWrapper(typeof(CompensableActivity)));
+            transation.Add(new ToolboxItemWrapper(typeof(Compensate)));
+            transation.Add(new ToolboxItemWrapper(typeof(Confirm)));
+            transation.Add(new ToolboxItemWrapper(typeof(TransactionScope)));
+            collection.Add(new ToolboxItemWrapper(typeof(AddToCollection<>)));
+            collection.Add(new ToolboxItemWrapper(typeof(ClearCollection<>)));
+            collection.Add(new ToolboxItemWrapper(typeof(ExistsInCollection<>)));
+            collection.Add(new ToolboxItemWrapper(typeof(RemoveFromCollection<>)));
+            errorHandling.Add(new ToolboxItemWrapper(typeof(Rethrow)));
+            errorHandling.Add(new ToolboxItemWrapper(typeof(Throw)));
+            errorHandling.Add(new ToolboxItemWrapper(typeof(TryCatch)));
 
             // Add the category to the ToolBox control.
             ctrl.Categories.Add(eisCategory);
-            ctrl.Categories.Add(category);
+            ctrl.Categories.Add(controlCategory);
+            ctrl.Categories.Add(flowCategory);
+            ctrl.Categories.Add(runtimeCategory);
+            ctrl.Categories.Add(primitivesCategory);
+            ctrl.Categories.Add(transation);
+            ctrl.Categories.Add(collection);
+            ctrl.Categories.Add(errorHandling);
+
             return ctrl;
         }
 
@@ -132,50 +183,100 @@ namespace WorkflowDesignerApp
 
         #endregion
 
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "XAML File (*.xaml)|*.xaml|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    wd = new WorkflowDesigner();
+                    Grid.SetColumn(wd.View, 1);
+                    wd.Load(openFileDialog.FileName);
+                    grid1.Children.Add(wd.View);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void BtnRun_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "XAML File (*.xaml)|*.xaml|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    ActivityBuilder ab;
+
+                    string readFileString = System.IO.File.ReadAllText(openFileDialog.FileName);
+                    ab = XamlServices.Load(
+                         ActivityXamlServices.CreateBuilderReader(
+                         new XamlXmlReader(new StringReader(readFileString)))) as ActivityBuilder;
+
+                    // XamlServices를 이용한 방법 - 이 경우 <, > 가 &lt, &gt로 치환되서 저장된다.
+//                  using (var stream = System.IO.File.OpenRead(OpenFileDialog.FileName))
+//                  {
+//                      ab = XamlServices.Load(
+//                           ActivityXamlServices.CreateBuilderReader(
+//                           new XamlXmlReader(new StringReader(XamlServices.Load(stream).ToString())))) as ActivityBuilder;
+//                  }
+
+                    // 워크플로 실행
+                    if (ab != null)
+                    {
+                        WorkflowApplication workflowApplication = null;
+                        try
+                        {
+                            workflowApplication = new WorkflowApplication(ab.Implementation);
+                            workflowApplication.Run();
+                            MessageBox.Show("Workflow completed.");
+                        }
+                        finally
+                        {
+                            if (workflowApplication != null)
+                            {
+                                workflowApplication.Abort();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to load workflow.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
             var saveFileDialog = new Microsoft.Win32.SaveFileDialog();
             saveFileDialog.Filter = "XAML File (*.xaml)|*.xaml|All files (*.*)|*.*";
             if (saveFileDialog.ShowDialog() == true)
             {
-                wd.Save(saveFileDialog.FileName);
-            }
-        }
+                try
+                {
+                    wd.Flush();
+                    File.WriteAllText(saveFileDialog.FileName, wd.Text);
 
-        private void BtnRun_Click(object sender, RoutedEventArgs e)
-        {
-            var OpenFileDialog = new Microsoft.Win32.OpenFileDialog();
-            OpenFileDialog.Filter = "XAML File (*.xaml)|*.xaml|All files (*.*)|*.*";
-            if (OpenFileDialog.ShowDialog() == true)
-            {
-                // XAML 파일에서 워크플로 로드
-                Activity workflow;
-                using (var stream = System.IO.File.OpenRead(OpenFileDialog.FileName))
-                {
-                    workflow = XamlServices.Load(stream) as Activity;
+                    // XamlServices를 이용한 방법 - 이 경우 <, > 가 &lt, &gt로 치환되서 저장된다.
+//                  StreamWriter sw = File.CreateText(saveFileDialog.FileName);
+//                  XamlWriter xw = ActivityXamlServices.CreateBuilderWriter(new XamlXmlWriter(sw, new XamlSchemaContext()));
+//                  wd.Flush();
+//                  XamlServices.Save(xw, wd.Text);
+//                  sw.Close();
                 }
-
-                // 워크플로 실행
-                if (workflow != null)
+                catch (Exception ex)
                 {
-                    WorkflowApplication workflowApplication = null;
-                    try
-                    {
-                        workflowApplication = new WorkflowApplication(workflow);
-                        workflowApplication.Run();
-                        MessageBox.Show("Workflow completed.");
-                    }
-                    finally
-                    {
-                        if (workflowApplication != null)
-                        {
-                            workflowApplication.Abort();
-                        }
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Failed to load workflow.");
+                    MessageBox.Show(ex.ToString());
                 }
             }
         }
